1b749d20bbc298f17ee2404ffd2b8507eb898e98
ZackarySantana <64446617+ZackarySantana@users.noreply.github.com>
2025-06-08 17:54:03 -0400

    feat: dummy sidebar

--- canopy/app/components/sidebar/sidebar.tsx
+++ canopy/app/components/sidebar/sidebar.tsx
@@ -1,17 +1,73 @@
+import { Calendar, Home, Inbox, Search, Settings } from "lucide-react";
 import {
     Sidebar as ShadSidebar,
     SidebarContent,
     SidebarFooter,
     SidebarGroup,
+    SidebarGroupContent,
+    SidebarGroupLabel,
     SidebarHeader,
+    SidebarMenu,
+    SidebarMenuBadge,
+    SidebarMenuButton,
+    SidebarMenuItem,
     SidebarProvider,
     SidebarTrigger,
 } from "../ui/sidebar";
 
+const items = [
+    {
+        title: "Home",
+        url: "/",
+        icon: Home,
+    },
+    {
+        title: "Inbox",
+        url: "/inbox",
+        icon: Inbox,
+    },
+    {
+        title: "Calendar",
+        url: "/calendar",
+        icon: Calendar,
+    },
+    {
+        title: "Search",
+        url: "/search",
+        icon: Search,
+    },
+    {
+        title: "Settings",
+        url: "/settings",
+        icon: Settings,
+    },
+];
+
 export function Sidebar({ children }: { children: React.ReactNode }) {
     return (
         <SidebarProvider>
-            <ShadSidebar>
+            <ShadSidebar collapsible="icon">
+                <SidebarHeader>Overstory</SidebarHeader>
+                <SidebarContent>
+                    <SidebarGroup>
+                        <SidebarGroupLabel>Application</SidebarGroupLabel>
+                        <SidebarGroupContent>
+                            <SidebarMenu>
+                                {items.map((item) => (
+                                    <SidebarMenuItem key={item.title}>
+                                        <SidebarMenuButton asChild>
+                                            <a href={item.url}>
+                                                <item.icon />
+                                                <span>{item.title}</span>
+                                            </a>
+                                        </SidebarMenuButton>
+                                        <SidebarMenuBadge>24</SidebarMenuBadge>
+                                    </SidebarMenuItem>
+                                ))}
+                            </SidebarMenu>
+                        </SidebarGroupContent>
+                    </SidebarGroup>
+                </SidebarContent>
                 <SidebarHeader />
                 <SidebarContent>
                     <SidebarGroup />
